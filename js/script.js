34cdeb5f566eb0849c3c59cab5ddfb89d2a2993d
Beth Sims <128319479+BethSims@users.noreply.github.com>
2023-05-27 11:36:22 -0500

    Completed assign items button code

--- js/script.js
+++ js/script.js
@@ -10,36 +10,83 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+// assign button
+const assignButton = document.querySelector(".assign");
+// assigned items list
+const assignedItems = document.querySelector(".assigned-items");
 
-addGuestButton.addEventListener("click", function() {
-    const guest = guestInput.value;
-    // console.log(guest);
-
-    if (guest !== "") {
-        addToList(guest);
-        clearInput();
-        updateGuestCount();
-    }
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    addGuest();
+  }
 });
 
-const clearInput = function() {
-    guestInput.value = "";
+addGuestButton.addEventListener("click", function () {
+  addGuest();
+});
+
+const addGuest = function () {
+  const guest = guestInput.value;
+  // console.log(guest);
+
+  if (guest !== "") {
+    addToList(guest);
+    clearInput();
+    updateGuestCount();
+  }
+};
+
+const clearInput = function () {
+  guestInput.value = "";
 };
 
 const addToList = function (guest) {
-    const listItem = document.createElement("li");
-    listItem.innerText = guest;
-    guestList.append(listItem);
+  const listItem = document.createElement("li");
+  listItem.innerText = guest;
+  guestList.append(listItem);
 };
 
 const updateGuestCount = function () {
-    const guests = document.querySelectorAll(".guest-list li");
-    guestCount.innerText = guests.length;
-    
-    if (guests.length === 8) {
-        addGuestButton.classList.add("hide");
-        guestInput.classList.add("hide");
-        guestInputLabel.classList.add("hide");
-        guestFull.classList.remove("hide");
-    }
-  };
+  const guests = document.querySelectorAll(".guest-list li");
+  guestCount.innerText = guests.length;
+
+  if (guests.length === 8) {
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    guestInputLabel.classList.add("hide");
+    guestFull.classList.remove("hide");
+  }
+};
+
+const assignItems = function () {
+  const potluckItems = [
+    "potato salad",
+    "baked beans",
+    "fruit tray",
+    "jell-o",
+    "chips and salsa",
+    "soda",
+    "lemonade",
+    "hot dogs",
+    "pasta salad",
+    "cole slaw",
+    "cheesecake"
+  ];
+  const allGuests = document.querySelectorAll(".guest-list li");
+
+  for (let guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    let randomPotluckItem = potluckItems[randomPotluckIndex];
+
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItems.append(listItem);
+
+    potluckItems.splice(randomPotluckIndex, 1);
+  }
+};
+
+assignButton.addEventListener("click", function () {
+  assignItems();
+  assignButton.disabled = true;
+});
